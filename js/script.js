99dbbfa77012c1c657238a8d981488530b1211fe
Gabriel <gabriel.gracilianomb@gmail.com>
2022-09-13 00:50:03 -0300

    Script adicionado com a lógica de adicionar jogadores

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const tabelaCorpo = document.querySelector(".tabela__corpo");
+const inputJogador = document.querySelector(".controle__input");
+const botaoAdicionaJogador = document.querySelector(".controle__adicionar");
+const gordon = {
+    nome: "Gordon",
+    pontos: 56,
+    baixas: 45,
+    latencia: 200,
+    voz: true
+};
+const shepard = {
+    nome: "Shepard",
+    pontos: 5,
+    baixas: 2,
+    latencia: 87,
+    voz: false
+};
+const listaJogadores = [gordon, shepard];
+
+botaoAdicionaJogador.addEventListener("click", () => {
+    criaNovoJogador(inputJogador.value);
+});
+
+function criaNovoJogador(nomeJogador) {
+    const jogador = {
+        nome: nomeJogador,
+        pontos: 0,
+        baixas: 0,
+        latencia: 0,
+        voz: false
+    }
+    adicionaJogador(jogador);
+    listaJogadores.push(jogador);
+}
+
+function adicionaJogador(jogador) {
+    tabelaCorpo.innerHTML += 
+    `<tr>
+        <td>${jogador.nome}</td>
+        <td>${jogador.pontos}</td>
+        <td>${jogador.baixas}</td>
+        <td>${jogador.latencia}</td>
+        <td>${adicionaIconeDeVoz(jogador.voz)}</td>
+    </tr>`;
+}
+
+function adicionaIconeDeVoz(bool) {
+    if(bool) {
+        return "🔊";
+    }
+    return "🔈";
+}
